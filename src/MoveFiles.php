bc30c82416d22282b93935306e68f9ea54ead3ff
sauvank <kevin.sauvanaud@epitech.eu>
2020-10-07 20:12:01 +0200

    fixe folder remove space not allow

--- src/MoveFiles.php
+++ src/MoveFiles.php
@@ -13,14 +13,8 @@ class MoveFiles extends GetFile
      * @return array
      */
     public function move($src, $output, bool $createOutputPath = true): array {
-        $mime = false;
-        try {
-           $mime =  mime_content_type($src);
-        }catch (\Exception $e){
-            return ['error' => true, 'msg' => 'File ' . $src . ' not exist !'];
-        }
 
-        if(!is_string($mime)){
+        if(!is_file($src)){
             return ['error' => true, 'msg' => 'File ' . $src . ' not exist !'];
         }
 
@@ -29,13 +23,15 @@ class MoveFiles extends GetFile
         }
 
         $SliceOutput = $this->sliceNameFileFromPath($output);
+
         if(!$SliceOutput['file_name']){
             return ['error' => true, 'msg' => 'Output path contain any file name !'];
         }
 
-        if($createOutputPath && !is_dir($SliceOutput['folder_path'])){
-            var_dump($SliceOutput['folder_path']);
-            $isCreate = mkdir($SliceOutput['folder_path'], 0700, true);
+        $folder = $this->replaceNotAllowedCharWindows($SliceOutput['folder_path']);
+
+        if($createOutputPath && !is_dir($folder)){
+            $isCreate = mkdir($folder, 0700, true);
 
             if(!$isCreate){
                 return ['error' => true, 'msg' => 'Folder output not created !'];
